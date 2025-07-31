c768f0f2529ad1ca6420879cede9ff60a4ff83ef
Ivan Skorokhodov <iskorokhodov@gmail.com>
2025-07-30 17:43:14 -0700

    feat (sds/msdl): augment batches with stream names

--- sds/msdl.py
+++ sds/msdl.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import math
 from typing import Any, Iterator
+from dataclasses import dataclass, field
 
 import numpy as np
 import torch
@@ -9,24 +10,56 @@ from sds.utils import misc
 
 #----------------------------------------------------------------------------
 
+STREAM_NAME_BATCH_KEY = '__stream_name__'
+
+#----------------------------------------------------------------------------
+# Helper enums and structs.
+
+class ScheduleType(Enum):
+    RANDOM = 'random'
+    ROUND_ROBIN = 'round_robin'
+    SHUFFLED_ROUND_ROBIN = 'shuffled_round_robin'
+
+    @classmethod
+    def from_str(cls, schedule: str) -> 'ScheduleType':
+        if schedule == 'random':
+            return cls.RANDOM
+        elif schedule == 'round_robin':
+            return cls.ROUND_ROBIN
+        elif schedule == 'shuffled_round_robin':
+            return cls.SHUFFLED_ROUND_ROBIN
+        else:
+            raise ValueError(f"Unsupported schedule: {schedule}. Supported schedules are 'random', 'round_robin', and 'shuffled_round_robin'.")
+
+@dataclass(frozen=True)
+class StreamConfig:
+    dataset: torch.utils.data.Dataset
+    weight: float | None = None
+    is_main: bool = False
+    kwargs: dict[str, Any] = field(default_factory=dict)
+    name: str | None = None
+
+#----------------------------------------------------------------------------
+
 class MultiStreamDataLoader:
     def __init__(
             self,
-            stream_cfgs: list[dict[str, Any]],
+            stream_configs: list[StreamConfig | dict[str, Any]],
             num_workers: int = 0,
             shuffle_seed: int | None = 42,
             schedule: str = 'shuffled_round_robin',
             **common_dataloader_kwargs,
         ):
-        assert schedule in ['random', 'shuffled_round_robin'], f"Unsupported schedule: {schedule}. Supported schedules are 'random' and 'shuffled_round_robin'."
-        assert num_workers == 0 or num_workers >= len(stream_cfgs), f"num_workers ({num_workers}) must be 0 or at least the number of streams ({len(stream_cfgs)})."
-        assert sum(s.get('weight') is None for s in stream_cfgs) in [0, len(stream_cfgs)], "All streams must either have a weight or none of them should have a weight."
+        stream_configs = [StreamConfig(**s) if isinstance(s, dict) else s for s in stream_configs]
+        assert schedule in ['random', 'round_robin', 'shuffled_round_robin'], f"Unsupported schedule: {schedule}. Supported schedules are 'random', 'round_robin', and 'shuffled_round_robin'."
+        assert num_workers == 0 or num_workers >= len(stream_configs), f"num_workers ({num_workers}) must be 0 or at least the number of streams ({len(stream_configs)})."
+        assert sum(s.weight is None for s in stream_configs) in [0, len(stream_configs)], "All streams must either have a weight or none of them should have a weight."
 
         # Computing the stream probabilities and filtering out streams with zero weight.
-        weights = np.array([s.get('weight', 1) for s in stream_cfgs])
+        weights = np.array([s.weight if s.weight is not None else 1 for s in stream_configs])
         assert weights.sum() > 0, "Weights must sum to a positive value."
         non_zero_stream_idx = np.where(weights > 0)[0]
-        stream_cfgs = [stream_cfgs[i] for i in non_zero_stream_idx]
+        self.stream_configs = [stream_configs[i] for i in non_zero_stream_idx]
         weights = weights[non_zero_stream_idx]
         self.probabilities = weights / weights.sum()
         self.counts = misc.probabilities_to_counts(self.probabilities)
@@ -34,7 +67,7 @@ class MultiStreamDataLoader:
         # We know have the notion of a "mini-epoch", for which we iterate over all the streams.
         self.epoch_size = sum(self.counts)
         self.shuffle_seed = shuffle_seed
-        self.schedule = schedule
+        self.schedule: ScheduleType = ScheduleType.from_str(schedule)
         assert self.schedule == 'random' or self.epoch_size < 1_000_000, f"TODO: we have a poor implementation of shuffled_round_robin."
 
         # Computing worker counts for each stream in a way that they sum to `num_workers`.
@@ -46,13 +79,14 @@ class MultiStreamDataLoader:
             assert worker_counts[largest_stream_idx] > 0, f"Impossible worker counts: {worker_counts}"
             worker_counts[largest_stream_idx] -= 1
 
-        self.dataloaders = [torch.utils.data.DataLoader(dataset=cfg['dataset'], **cfg.get('kwargs', {}), **common_dataloader_kwargs, num_workers=nw) for cfg, nw in zip(stream_cfgs, worker_counts)]
+        self.dataloaders = [torch.utils.data.DataLoader(dataset=cfg.dataset, **cfg.kwargs, **common_dataloader_kwargs, num_workers=nw) for cfg, nw in zip(stream_configs, worker_counts)]
         self.streams = [inf_loop_dataloader(dl) for dl in self.dataloaders]
+        self._main_stream_idx = next(i for i, cfg in enumerate(stream_configs) if cfg.is_main) if any(cfg.is_main for cfg in stream_configs) else np.argmax(self.probabilities)
 
     @property
     def main_stream(self) -> Iterator:
         """Gets the stream with the highest weight."""
-        return self.streams[np.argmax(self.probabilities)]
+        return self.streams[self._main_stream_idx]
 
     def state_dict(self) -> dict[str, Any]:
         stream_states = [dl.dataset.state_dict() if hasattr(dl, 'dataset') and hasattr(dl.dataset, 'state_dict') else None for dl in self.dataloaders]
@@ -66,19 +100,31 @@ class MultiStreamDataLoader:
             elif state is not None:
                 raise ValueError("Stream dataset does not support loading state dict.")
 
+    def _post_process_batch(self, batch: dict[str, Any], stream_idx: int) -> dict[str, Any]:
+        f"""Adds a special {STREAM_NAME_BATCH_KEY} key to each element in the batch."""
+        # TODO: we assume that the batch is a dict and has already been collated.
+        batch_size = len(next(iter(batch.values())))
+        stream_name = self.stream_configs[stream_idx].name or f'stream_{stream_idx:03d}'
+        batch[STREAM_NAME_BATCH_KEY] = [stream_name] * batch_size
+        return batch
+
     def __iter__(self) -> Any:
         num_batches_yielded = 0
 
         while True:
-            if self.schedule == 'shuffled_round_robin':
+            if self.schedule in [ScheduleType.ROUND_ROBIN, ScheduleType.SHUFFLED_ROUND_ROBIN]:
                 cur_plan: list[int] = sum([[i] * c for i, c in enumerate(self.counts)], start=[]) # Counts to counted idx: [1,2,3] => [0,1,1,2,2,2]
-                cur_plan: list[int] = np.random.RandomState(num_batches_yielded + self.shuffle_seed).permutation(cur_plan)
+                if self.schedule == ScheduleType.SHUFFLED_ROUND_ROBIN:
+                    np.random.RandomState(num_batches_yielded + self.shuffle_seed).shuffle(cur_plan)
                 for stream_idx in cur_plan:
-                    yield next(self.streams[stream_idx])
-            elif self.schedule == 'random':
+                    yield self._post_process_batch(next(self.streams[stream_idx]), stream_idx)
+                    num_batches_yielded += 1
+            elif self.schedule == ScheduleType.RANDOM:
                 stream_idx = np.random.RandomState(num_batches_yielded + self.shuffle_seed).choice(len(self.streams), p=self.probabilities)
-                yield next(self.streams[stream_idx])
+                yield self._post_process_batch(next(self.streams[stream_idx]), stream_idx)
                 num_batches_yielded += 1
+            else:
+                raise ValueError(f"Unsupported schedule: {self.schedule}. Supported schedules are 'random', 'round_robin', and 'shuffled_round_robin'.")
 
 #----------------------------------------------------------------------------
 # Helper dataloading functions.
