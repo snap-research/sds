0a860b59de8522f20b503f2d44cdacf733cca068
Ivan Skorokhodov <iskorokhodov@gmail.com>
2025-08-05 10:01:45 -0700

    feat (sds/__init__): disable logging by default

---
+++ sds/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+from loguru import logger
+
+logger.disable("sds") # By default, disable all loggers whose names start with "sds".
+env_log_level = os.getenv("SDS_LOG_LEVEL") # Check for the user-defined environment variable.
+
+if env_log_level:
+    try:
+        logger.remove(0)
+    except ValueError:
+        pass
+
+    log_level = env_log_level.upper()
+    logger.add(sys.stderr, level=log_level)
+    logger.enable("sds")
+    logger.info(f"Logging for 'sds' and its submodules enabled at '{log_level}' level.")
